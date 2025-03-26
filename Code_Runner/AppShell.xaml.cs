a91682f8bc18f56f0dcec47be3e861b381c1e54d
Dembolx <106410918+Dembolx@users.noreply.github.com>
2025-03-26 12:10:14 +0100

    nowy projekt

---
+++ Code_Runner/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Code_Runner
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Code_Runner/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Code_Runner
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
